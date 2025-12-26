a2f9eabf1cf30fe4e7f1ec9dcf70448347296179
jspinerola <144980273+jspinerola@users.noreply.github.com>
2025-12-26 16:26:45 -0600

    feat: made project cards

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button";
+import { ArrowUpRight } from "lucide-react";
 
 type BaseButtonProps = Parameters<typeof Button>[0];
 type ButtonProps = Omit<BaseButtonProps, "asChild">;
@@ -7,13 +8,18 @@ interface LinkProps extends ButtonProps {
   href: string;
   children: React.ReactNode;
   target?: string;
+  showIcon?: boolean;
 }
-
-export default function LinkButton({ href, children, ...props }: LinkProps) {
+export default function LinkButton({
+  href,
+  children,
+  showIcon = true,
+  ...props
+}: LinkProps) {
   return (
     <Button asChild {...props}>
-      <a href={href} target={props.target}>
-        {children}
+      <a href={href} target={props.target} className="font-mono lowercase">
+        {children} {showIcon && <ArrowUpRight />}
       </a>
     </Button>
   );
